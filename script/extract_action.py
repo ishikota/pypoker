cb734838cd74b1b29a8a0aebaef3169eac5b94f1
Kota Ishimoto <ishikota086@gmail.com>
2015-07-31 18:02:06 +0900

    changed data structure of p_dict to enable holding multiple same player action

--- script/extract_action.py
+++ script/extract_action.py
@@ -7,6 +7,7 @@ path_to_data = cwd[:-len("script/extract_street.py")]+"data/"
 import pdb
 import re
 import csv
+import copy
 import data_processor
 
 # return list of
@@ -20,7 +21,7 @@ def readPlayersInfo(stage_info):
   for item in stage_info:
     if isinstance(item, str) or item[0] == 'sb' or item[0] == 'bb':
       continue
-    res[item[1]] = ['?', [0,0,0], 0, float(item[2]), player_num]
+    res[item[1]] = {'act':[], 'stack':float(item[2])}
   return res
 
 def extractAction(act):
@@ -47,16 +48,18 @@ def readActions(p_dict, street_data):
   for pid, act in street_data:
     if act[:8] == 'returned': continue
     if pid not in p_dict: continue
-    d = p_dict[pid]
     action, chip = extractAction(act)
+    d = ['?', [0,0,0], 0, p_dict[pid]['stack'], p_dict['active']]
     d[0] = action
-    d[4] = p_dict['active']
     for i in range(3): d[1][i] = action_holder[i]
     d[2] = pot
+    d[3] -= chip
+    d[4] = p_dict['active']
     action_holder[act_index[action]] += 1
     if action == 'Folds': p_dict['active'] -=1
     if p_dict['active'] < 0 : pdb.set_trace()
-    p_dict[pid][3] -= chip
+    p_dict[pid]['act'].append(d)
+    p_dict[pid]['stack'] = d[3]
     pot += chip
   p_dict['pot'] = pot
 
@@ -83,15 +86,67 @@ def toCSV(data, key):
 def main():
   formatter = data_processor.Formatter()
   data = formatter.format(path_to_data+"text/in.txt")
-  res = []
+  res = {'PREFLOP':[],'FLOP':[],'TURN':[],'RIVER':[]}
   for game_data in data:
-    p_dict = readPlayersInfo(game_data['Stage'])
+    # read preflopp actions
     pdb.set_trace()
+    p_dict = readPlayersInfo(game_data['Stage'])
     readActions(p_dict, game_data['PREFLOP'])
     for key in p_dict:
       if key in ['pot','actions','active']: continue
-      res.append(p_dict[key])
-  toCSV(res, 'player_action')
+      for act in p_dict[key]['act']:
+        res['PREFLOP'].append(act)
+
+    if 'FLOP' not in game_data: continue
+    # reset player action history in p_dict
+    p_dict2 = copy.deepcopy(p_dict)
+    p_dict2['actions'] = [0,0,0]
+    for key in p_dict2:
+      if key in ['pot','actions','active']: continue
+      p_dict2[key]['act'] = []
+    # read FLOP actions
+    readActions(p_dict2, game_data['FLOP'][1:]) # [1:] exclude board info
+    for key in p_dict2:
+      if key in ['pot','actions','active']: continue
+      for act in p_dict2[key]['act']:
+        res['FLOP'].append(act)
+
+
+    if 'TURN' not in game_data:continue
+    # reset player action history in p_dict
+    p_dict3 = copy.deepcopy(p_dict2)
+    p_dict3['actions'] = [0,0,0]
+    for key in p_dict3:
+      if key in ['pot','actions','active']: continue
+      p_dict3[key]['act'] = []
+    # read RIVER actions
+    readActions(p_dict3, game_data['TURN'][1:]) # [1:] exclude board info
+    for key in p_dict3:
+      if key in ['pot','actions','active']: continue
+      for act in p_dict3[key]['act']:
+          res['TURN'].append(act)
+
+    
+    if 'RIVER' not in game_data:continue
+    # reset player action history in p_dict
+    p_dict4 = copy.deepcopy(p_dict3)
+    p_dict4['actions'] = [0,0,0]
+    for key in p_dict4:
+      if key in ['pot','actions','active']: continue
+      p_dict4[key]['act'] = []
+    # read RIVER actions
+    readActions(p_dict4, game_data['RIVER'][1:]) # [1:] exclude board info
+    for key in p_dict4:
+      if key in ['pot','actions','active']: continue
+      for act in p_dict4[key]['act']:
+          res['RIVER'].append(act)
+
+
+  toCSV(res['PREFLOP'], 'action_preflop')
+  toCSV(res['FLOP'], 'action_flop')
+  toCSV(res['TURN'], 'action_turn')
+  toCSV(res['RIVER'], 'action_river')
+
 
 if __name__=='__main__':
   main()

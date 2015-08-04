ee937fa534c13df21b95f51ea99b01159d84e47c
Kota Ishimoto <ishikota086@gmail.com>
2015-08-04 11:04:03 +0900

    added debug mode on MockPlayer which display its action.

--- engine/players/mock_player.py
+++ engine/players/mock_player.py
@@ -11,6 +11,7 @@ class MockPlayer(BasePlayer):
         super(MockPlayer, self).__init__(pid, player_name, start_stack)
         self.act_index = 0
         self.actions = []
+        self.D = False  # debug flg
 
     def set_action(self, actions):
         self.actions = actions
@@ -18,4 +19,5 @@ class MockPlayer(BasePlayer):
     def action(self, info):
         action = self.actions[self.act_index%len(self.actions)]
         self.act_index += 1
+        if self.D: print "{0}:{1}".format(self.NAME, action)
         return action

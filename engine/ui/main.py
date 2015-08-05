744da5ecb76f84271995c763229f1ea9017a1fc3
Kota Ishimoto <ishikota086@gmail.com>
2015-08-05 16:24:12 +0900

    added script to run the poker game

---
+++ engine/ui/main.py
@@ -0,0 +1,29 @@
+"""
+
+    To run this script, you need to add root path of this project
+    to your PYTHONPATH.
+
+    If you put this root project to /Users/YourName/poker 
+    then run below two command on your terminal.
+
+    PYTHONPATH=/Users/YourName/poker:$PYTHONPATH
+    export PYTHONPATH
+
+"""
+import sys, os
+
+from engine.ui.table import Table
+from engine.ui.config import Config
+
+# read game config
+C = Config()
+path_to_config = os.path.abspath(__file__)[:-len("main.py")]
+C.read(path_to_config+"config.txt")
+
+# set up the game
+p = C.getPlayers()
+t = Table()
+t.setup(p, C.getSBChip())
+
+# start the game !!
+t.start_game(C.getRoundNum())
